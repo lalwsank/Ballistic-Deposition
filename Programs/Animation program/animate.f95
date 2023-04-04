9b1a98e0baf9facf2190ace0a1a58bf530e05632
lalwsank <123665370+lalwsank@users.noreply.github.com>
2023-04-04 13:03:28 +0530

    Organizing

--- Programs/Animation program/animate.f95
+++ Programs/Animation program/animate.f95
@@ -1,8 +1,8 @@
 program BD_Animation
 	implicit none
-	integer, allocatable :: A(:,:)
+	real, allocatable :: A(:,:)
 	integer :: nmc, length, i, j
-
+	
 	open(1,file="final.dat",status = "old")
 	open(2,file="animate.dat")
 
@@ -11,15 +11,15 @@ program BD_Animation
 	write(*,*)"Enter the Length of the Box:"
 	read(*,*)Length
 	
-	allocate(A(nmc*length,2))
+	allocate( A(nmc*length, 2) ); A = 0.0
 	
 	do i = 1,nmc*length
-		read(1,*)A(i,1), A(i,2)
+		read(1,*) A(i,1), A(i,2)
 	enddo
 	
 	do i = 1,nmc
 		do j = 1,length*i
-		write(2,*)A(i+j-1,1),A(i+j-1,2)
+			write(2,*) A(j,1), A(j,2)
 		enddo	
 		write(2,*)" "
 		write(2,*)" "
